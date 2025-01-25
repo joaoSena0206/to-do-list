745b06819b9acc5a5055184388084b9869a1c3c1
João Sena <joaosena0206@gmail.com>
2025-01-25 11:47:28 -0300

    feat: adiciona classe Database para a camada DAL

---
+++ To-do List/DataAccess/Database.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace To_do_List.DataAccess
+{
+    public class Database
+    {
+        private string _connectionString;
+
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void ExecuteCommand(string commandString, List<SqlParameter>? parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(commandString, connection);
+
+                if (parameters != null)
+                {
+                    foreach (SqlParameter param in parameters)
+                    {
+                        command.Parameters.Add(param);
+                    }
+                }
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public SqlDataReader ExecuteQuery(string commandString, List<SqlParameter>? parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(commandString, connection);
+
+                if (parameters != null)
+                {
+                    foreach (SqlParameter param in parameters)
+                    {
+                        command.Parameters.Add(param);
+                    }
+                }
+
+                return command.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+        }
+    }
+}
