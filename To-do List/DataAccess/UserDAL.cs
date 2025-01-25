2c6c4c43a9f9a2704f684f74f46cdaa072a64ad6
João Sena <joaosena0206@gmail.com>
2025-01-25 12:31:58 -0300

    feat: adiciona método para inserir o usuário no banco

---
+++ To-do List/DataAccess/UserDAL.cs
@@ -0,0 +1,36 @@
+﻿using BCrypt.Net;
+using Microsoft.Data.SqlClient;
+using To_do_List.Models;
+
+namespace To_do_List.DataAccess
+{
+    public class UserDAL
+    {
+        private readonly Database _database;
+
+        public UserDAL(string connectionString)
+        {
+            Database database = new Database(connectionString);
+            _database = database;
+        }
+
+        public void CreateUser(User user)
+        {
+            // Efetua o hash da senha
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+            // Insere no banco o usuário
+            string comando = @"
+            INSERT INTO usuario VALUES (
+	            @Email,
+	            @Password
+            );";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("Email", user.Email));
+            parameters.Add(new SqlParameter("Password", user.Password));
+
+            _database.ExecuteCommand(comando, parameters);
+        }
+    }
+}
