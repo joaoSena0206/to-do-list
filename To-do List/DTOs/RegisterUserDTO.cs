b6031cb03002fbe86063029f4cbc4c8778d89b3e
João Sena <joaosena0206@gmail.com>
2025-01-25 15:20:55 -0300

    feat: adiciona DTO para registrar o usuário

---
+++ To-do List/DTOs/RegisterUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace To_do_List.DTOs
+{
+    public class RegisterUserDTO
+    {
+        [Required(ErrorMessage = "Email é obrigatório!")]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatória!")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
--- To-do List/Models/User.cs
+++ To-do List/Models/User.cs
@@ -5,15 +5,8 @@ namespace To_do_List.Models
 {
     public class User
     {
-        [Key]
-        [Required]
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
-
         public List<Task> Tasks { get; set; }
 
         public User()
