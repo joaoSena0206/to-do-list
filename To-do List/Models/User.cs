f45b7e5b10d709f1671838849915397008cd3229
João Sena <joaosena0206@gmail.com>
2025-01-25 11:59:02 -0300

    feat: adiciona modelo User

---
+++ To-do List/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace To_do_List.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
