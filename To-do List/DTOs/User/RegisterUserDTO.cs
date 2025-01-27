558b03769813d24ae3de6f3ef3e3c40fb7dedcf7
João Sena <joaosena0206@gmail.com>
2025-01-27 15:02:44 -0300

    style: remove atributo nulável por ter mudado configuração do projeto

--- To-do List/DTOs/User/RegisterUserDTO.cs
+++ To-do List/DTOs/User/RegisterUserDTO.cs
@@ -7,15 +7,15 @@ namespace To_do_List.DTOs.User
         [Required(ErrorMessage = "Email é obrigatório!")]
         [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "Email inválido!")]
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Senha é obrigatória!")]
         [DataType(DataType.Password)]
-        public string? Password { get; set; }
+        public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirme a senha!")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "As senhas não são iguais!")]
-        public string? ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
