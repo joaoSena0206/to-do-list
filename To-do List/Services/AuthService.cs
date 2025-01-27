7451e95b1f53ff3be47fe358dbc0a3caeb033b5f
João Sena <joaosena0206@gmail.com>
2025-01-27 15:03:37 -0300

    feat: adiciona serviço de autenticação para validar os dados do usuário e gerar o token JWT

---
+++ To-do List/Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using To_do_List.Configurations;
+
+namespace To_do_List.Services
+{
+    public class AuthService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthService(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(string email)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, email)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken tokenDescriptor = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
