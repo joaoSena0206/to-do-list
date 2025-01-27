4194099989a1f240e19dacde814061174cfe3742
João Sena <joaosena0206@gmail.com>
2025-01-27 15:24:42 -0300

    feat: adiciona requisição para logar o usuário

---
+++ To-do List/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using To_do_List.DTOs.User;
+using To_do_List.Services;
+
+namespace To_do_List.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public IActionResult SignInUser(LoginUserDTO user)
+        {
+            try
+            {
+                string token = _authService.ValidateUser(user);
+
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
