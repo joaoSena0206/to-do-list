3c6172b0d760a16281ce85d6504f24f9a060869d
João Sena <joaosena0206@gmail.com>
2025-01-27 15:24:14 -0300

    feat: adiciona método de validação do login do usuário

--- To-do List/Services/AuthService.cs
+++ To-do List/Services/AuthService.cs
@@ -4,19 +4,40 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using To_do_List.Configurations;
+using To_do_List.DataAccess;
+using To_do_List.DTOs.User;
 
 namespace To_do_List.Services
 {
     public class AuthService
     {
         private readonly JwtSettings _jwtSettings;
+        private readonly UserDAL _userDAL;
 
-        public AuthService(IOptions<JwtSettings> jwtSettings)
+        public AuthService(IOptions<JwtSettings> jwtSettings, UserDAL userDAL)
         {
             _jwtSettings = jwtSettings.Value;
+            _userDAL = userDAL;
         }
 
-        public string GenerateToken(string email)
+        public string ValidateUser(LoginUserDTO user)
+        {
+            string passwordHash = _userDAL.GetPasswordHash(user.Email);
+
+            if (String.IsNullOrEmpty(passwordHash))
+            {
+                throw new Exception("Usuário não cadastrado!");
+            }
+
+            if (BCrypt.Net.BCrypt.Verify(user.Password, passwordHash))
+            {
+                return GenerateToken(user.Email);
+            }
+
+            throw new Exception("Usuário ou senha incorretas!");
+        }
+
+        private string GenerateToken(string email)
         {
             List<Claim> claims = new List<Claim>()
             {
