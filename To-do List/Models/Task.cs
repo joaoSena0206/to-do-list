d7b574b614532a1d8ea82e288a90f687c66dbd2f
João Sena <joaosena0206@gmail.com>
2025-01-28 18:08:31 -0300

    refactor: retira lógica de negócios do modelo

--- To-do List/Models/Task.cs
+++ To-do List/Models/Task.cs
@@ -1,22 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace To_do_List.Models
+﻿namespace To_do_List.Models
 {
     public class Task
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
         public User User { get; set; }
-
-        [Required]
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public DateTime DueDate { get; set; }
-
         public bool IsCompleted { get; set; }
 
         public Task() 
