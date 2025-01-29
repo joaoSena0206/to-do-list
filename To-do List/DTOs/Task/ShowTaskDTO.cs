6f5889114cd265f8479823e7aea502bb819c394c
João Sena <joaosena0206@gmail.com>
2025-01-29 11:04:12 -0300

    refactor: muda o tipo para string da data de vencimento para melhor formatação ao usuário

--- To-do List/DTOs/Task/ShowTaskDTO.cs
+++ To-do List/DTOs/Task/ShowTaskDTO.cs
@@ -5,13 +5,14 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime? DueDate { get; set; }
+        public string DueDate { get; set; }
         public bool IsCompleted { get; set; }
 
         public ShowTaskDTO()
         { 
             Title = string.Empty;
             Description = string.Empty;
+            DueDate = string.Empty;
         }
     }
 }
