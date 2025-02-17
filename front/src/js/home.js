b58f5d798b964c2afe8d5c23bece856a41479b1c
João Sena <joaosena0206@gmail.com>
2025-02-17 20:30:07 -0300

    feat: adiciona dropdown para filtragem da data da tarefa

--- front/src/js/home.js
+++ front/src/js/home.js
@@ -1,6 +1,7 @@
 $(function () {
     const datePicker = flatpickr("#datepicker", {
-        dateFormat: "m-d-Y",
+        dateFormat: "m-d-Y H:i",
+        enableTime: true,
     });
 
     $("#calendar").on("click", function () {
@@ -13,5 +14,15 @@ $(function () {
 
     $("#closeBtn").on("click", function () {
         $("#formTask").addClass("hidden").removeClass("flex");
+    });
+
+    $("#dropdownDays").on("click", function () {
+        if ($("#dropdownOptions").hasClass("hidden")) {
+            $("#dropdownOptions").removeClass("hidden");
+        }
+        else
+        {
+            $("#dropdownOptions").addClass("hidden");
+        }
     });
 });
