45ad8ddf1a50d03e40e605d9cde1a09d372176da
João Sena <joaosena0206@gmail.com>
2025-02-17 14:31:44 -0300

    feat: mostra o form da tarefa ao clicar no +

--- front/src/js/home.js
+++ front/src/js/home.js
@@ -3,7 +3,15 @@ $(function () {
         dateFormat: "d-m-Y",
     });
 
-    $("#calendar").on("click", function() {
+    $("#calendar").on("click", function () {
         datePicker.toggle();
+    });
+
+    $("#addTaskBtn").on("click", function () {
+        $("#formTask").removeClass("hidden").addClass("flex");
+    });
+
+    $("#closeBtn").on("click", function () {
+        $("#formTask").addClass("hidden").removeClass("flex");
     });
 });
