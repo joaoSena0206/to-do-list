50dd5d3183f0c877c0fe1dd99c7d816f9efcf8a6
João Sena <joaosena0206@gmail.com>
2025-02-20 17:38:34 -0300

    feat: adiciona função para adicionar tarefa

--- front/src/js/home.js
+++ front/src/js/home.js
@@ -66,4 +66,49 @@ $(function () {
             });
         }
     });
+
+    $("#createTaskBtn").on("click", function () {
+        let taskName = $("#inputTaskName").val();
+        let taskDesc = $("#inputTaskName").val();
+        let taskDate = $("#datepicker").val();
+
+        let div = document.createElement("div");
+        div.setAttribute("completed", "false");
+        div.setAttribute("taskId", "1");
+
+        div = $(div);
+
+        div.addClass("flex items-center space-x-3 bg-[#363636] rounded-sm px-2 py-3");
+
+        div.html(`
+            <div class="transition-colors duration-300 taskCompleteBtn border-[2px] border-white/87 w-[16px] h-[16px] rounded-full"></div>
+            <div class="text-white">
+                <h1 class="font-bold text-[15px]">${taskName}</h1>
+                <h2 class="text-[15px] text-[#AFAFAF]">Today at 16:45</h2>
+            </div>    
+        `);
+
+        div.children(".taskCompleteBtn").on("click", function (e) {
+            const task = $(this).parent();
+
+            if (task.attr("completed") == "false") {
+                $(this).addClass("bg-white/87");
+
+                task.attr("completed", "true");
+                task.slideUp(250, function () {
+                    $(this).detach().appendTo(sectionCompletedTasks).slideDown(250);
+                });
+            } else {
+                $(this).removeClass("bg-white/87");
+
+                task.attr("completed", "false");
+                task.slideUp(250, function () {
+                    $(this).detach().appendTo(sectionIncompleteTasks).slideDown(250);
+                });
+            }
+        });
+
+        sectionIncompleteTasks.append(div);
+        $("#formTask").addClass("hidden").removeClass("flex");
+    });
 });
